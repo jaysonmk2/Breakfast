cbb798a122d84810cfda3e86aff8cc2b52538e33
jaysonmk2 <76066308+jaysonmk2@users.noreply.github.com>
2021-07-29 21:33:38 -0700

    all pages added, still need to add links to pages

--- breakfast/recipies/urls.py
+++ breakfast/recipies/urls.py
@@ -1,10 +1,11 @@
 from django import urls
 from django.urls import path
-from .views import recipie_list
+from .views import recipie_list, recipies_detail
 
 
 app_name= "breakfast"
 
 urlpatterns = [
-    path('', recipie_list, name="recipies")
+    path('', recipie_list, name="recipies"),
+    path('<int:recipie_id>', recipies_detail, name='detail')
 ]
--- breakfast/recipies/views.py
+++ breakfast/recipies/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from .models import Recipies
 
 # Create your views here.
@@ -9,3 +9,8 @@ def recipie_list(request):
 
     recipie = Recipies.objects.all()
     return render(request, 'recipies/recipies.html', {'recipie' : recipie})
+
+
+def recipies_detail(request, recipie_id):
+    recipie_detail = get_object_or_404(Recipies, pk= recipie_id)
+    return render(request, 'details/details.html', {'detail':recipie_detail})
